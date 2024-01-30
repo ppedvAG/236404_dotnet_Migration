75a68e97a8d55ed1694fb5cb6caa8ccdd4d98697
André <AndreR@ppedv.de>
2024-01-30 09:46:30 +0100

    Parallel macht alles gleichzeitig

---
+++ HalloParallel/HalloParallel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HalloParallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo");
+
+            Parallel.For(0, 100000, i => Console.WriteLine(i));
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
